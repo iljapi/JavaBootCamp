ef9d42347bcdbd0ebaf4f74bfb1f9fd419f15337
iljapi <85032096+iljapi@users.noreply.github.com>
2021-05-31 20:47:46 +0100

    1

--- SEF/src/sef/module4/activity/CalculatorClient.java
+++ SEF/src/sef/module4/activity/CalculatorClient.java
@@ -10,28 +10,32 @@ public class CalculatorClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		/*Instantiate Normal Calculator. Note that CALC_TYPE_NORMAL is a public static
-		 *final variable defined in Calculator class. 
-		*/
+		 *final variable defined in Calculator class.
+		 */
 		Calculator normalCalc = new Calculator(Calculator.CALC_TYPE_BASIC);
-		
+
 		System.out.println("*** Calculations will be performed using "+normalCalc.getCalculatorType()+ " calculator ***");
-		
-		// Add 
+
+		// Add
 		int result = normalCalc.add(4, 5);
 		System.out.println("1) Add result is " + result);
-		
-				
-		// Multiply 
+
+
+		// Multiply
 		int[] mul = {2, 3, 4};
 		result = normalCalc.multiply(mul);
-		System.out.println("2) Multiplication result is " + result); 
-		
+		System.out.println("2) Multiplication result is " + result);
 
+		result = normalCalc.divide(4,2);
+		System.out.println("3) Divide result is " + result);
+
+		result = normalCalc.subtract(22, 3);
+		System.out.println("4) Subscract result is " + result);
 	}
 
 
-	
+
 
 }
