a8d9607074fb7eec98b0a9c6b7c42b5c9da5de8c
iljapi <85032096+iljapi@users.noreply.github.com>
2021-06-04 10:44:15 +0100

    5 module 9 map

--- SEF/src/sef/module9/activity/MapActivity.java
+++ SEF/src/sef/module9/activity/MapActivity.java
@@ -1,18 +1,24 @@
 package sef.module9.activity;
 //Needs to be completed
 
-import java.util.Map;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class MapActivity {
 
-	static String temp = "My Testing" ;
+	static String temp = "My Testing";
 
 	public static void main(String[] args) {
 		//1 - Type code to create a HashMap of key value pair
 		//where key is id of type String and value is a name
-		Map map = new HashMap(){
-
+		Map map = new HashMap();
+		map.put("A", "A String");
+		map.put("B", "B String");
+		map.put("C", "C String");
+		map.put("D", "D String");
 
 
 //		print(temp);
@@ -20,8 +26,29 @@ public class MapActivity {
 //		print2(temp);
 //		print3(temp);
 		//2 - Call print method to print the map passed as its parameter.
+		print(map);
 
 	}
+	static void print(Map map)
+	{
+		//Keys are maintained as set in map.
+		Set keySet = map.keySet();
+
+		//You can use an iterator to access keys
+		System.out.println("*************************************");
+		System.out.println("ID \tName");
+		Iterator it=keySet.iterator();
+		while(it.hasNext())
+		{
+			String key=(String)it.next();
+			System.out.println(key+"\t"+map.get(key));
+		}
+
+		System.out.println("*************************************");
+
+	}
+
+
 
 	static void print(String test)
 	{
